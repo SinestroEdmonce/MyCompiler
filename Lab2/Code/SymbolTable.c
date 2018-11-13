ae358f4ce37a62a637ffe73c125244453a60bae3
YinQiaoyi <867208887@qq.com>
2018-11-14 00:07:05 +0800

    Update: 1. Complete some functions required.

--- Lab2/Code/SymbolTable.c
+++ Lab2/Code/SymbolTable.c
@@ -11,7 +11,7 @@ SymbolNode* structure_hashtable[HASHTABLE_LENGTH + 1];
 SymbolNode* symbol_scope_stack[MAX_SCOPE_NUM + 1]; 
 static int scope_level = 0; 
 
-
+/* Recommended hashing method in the given guidance */
 unsigned int Hash_Method_PJW(char* name){
     unsigned int val = 0, idx;
     for (; *name; ++name){
@@ -21,3 +21,35 @@ unsigned int Hash_Method_PJW(char* name){
     }
     return val;
 }
+
+/* Look up a specific VARIABLE or FUNCTION symbol */
+SymbolNode* Find_Var_Func_Symbol(char* symbol_id){
+    /* Obtain hash value */
+    unsigned int loc_index = Hash_Method_PJW(symbol_id);
+
+    SymbolNode* pt = var_func_hashtable[loc_index];
+    while(pt!=NULL && strcmp(pt->var_func_symbol->record_name, symbol_id)!=0){
+        pt=pt->node_next;
+    }
+
+    if (pt!=NULL)
+        return pt->var_func_symbol;
+    else
+        return NULL;
+}
+
+/* Look up a specific STRUCTURE symbol */
+StructureSymbol *Find_Structure_Symbol(char* structure_symbol_id){
+    unsigned int loc_index = Hash_Method_PJW(structure_symbol_id);
+
+    SymbolNode* pt = structure_hashtable[loc_index];
+    while(pt!=NULL && strcmp(pt->structure_symbol->structure_name, structure_symbol_id)!=0){
+        pt=pt->node_next;
+    }
+
+    if(pt!=NULL)
+        return pt->structure_symbol;
+    else
+        return NULL;
+}
+
